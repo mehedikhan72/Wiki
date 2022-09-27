8b9b4a5fe9d3c1a46b81eb61773db0691b802592
mehedi khan <lopingcard@gmail.com>
2022-09-28 00:39:44 +0600

    added edit, random page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title_name>", views.title, name="title"),
-    path("new_page", views.new_page, name="new_page")
+    path("wiki/<str:title_name>/", views.title, name="title"),
+    path("new_page", views.new_page, name="new_page"),
+    path("edit_page", views.edit_page, name="edit_page"),
+    path("wiki/random", views.random_page, name="random_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from django import forms
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+import random
 
 from . import util
-from wiki import encyclopedia
 
 
 def index(request):
@@ -18,7 +18,6 @@ def index(request):
         else:
             sub_list = []
             main_list = util.list_entries()
-            print(main_list)
 
             for item in main_list:
                 if title_name.lower() in item.lower():
@@ -55,17 +54,18 @@ def new_page(request):
                 "message" : "Must provide a title."
             })
         
-        print(len(md_content))
         if not md_content:
             return render(request, "encyclopedia/error.html", {
                 "message" : "Must provide markdown content for the page."
             })
         
+
         existing_titles = util.list_entries()
-        if title_name in existing_titles:
-            return render(request, "encyclopedia/error.html",{
-                "message" : "Another page with the same title already exists. Please choose another one."
-            })
+        for item in existing_titles:
+            if title_name.lower() == item.lower():
+                return render(request, "encyclopedia/error.html",{
+                    "message" : "Another page with the same title already exists. Please choose another one."
+                })
 
         util.save_entry(title_name, md_content)
         #TODO: Redirecting user to home for now, but need to redirect user to the page they just created.
@@ -73,5 +73,39 @@ def new_page(request):
         
     return render(request, "encyclopedia/new_page.html")
 
-def edit_page():
-    return render(request, "encyclopedia/edit_page.html")
+def edit_page(request):
+    if request.method == "POST":
+        md_content = request.POST.get("md_content")
+        title_name = request.POST.get("title")
+
+        if not md_content:
+            return render(request, "encyclopedia/error.html",{
+                "message" : "Must provide md_content."
+            })
+        util.save_entry(title_name, md_content)
+        return redirect('/')
+        
+    pre_url = request.META.get('HTTP_REFERER')
+    title_name = ""
+    for i in range(len(pre_url)):
+        if i > 26 and i < len(pre_url) - 1:
+            title_name += pre_url[i]
+
+    md_content = util.get_entry(title_name)
+    return render(request, "encyclopedia/edit_page.html",{
+        "title_name" : title_name,
+        "md_content" : md_content
+    })
+
+
+# It is very logical to not let the user edit the page from random page cause if you wanted to edit 
+# a specific page, you wouldn't go to the random page.(but deep down, we all know I just couldn't figure a way out to do so.)
+
+def random_page(request):
+    existing_titles = util.list_entries()
+    random_title = random.choice(existing_titles)
+    print(random_title)
+    return render(request, "encyclopedia/entry2.html", {
+        "title_name" : random_title,
+        "entries" : util.get_entry(random_title)
+    })
